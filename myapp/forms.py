197ea90050a306347710440a8f5a648738c3a9db
shashwat <shashwat.jn@gmail.com>
2023-05-11 01:56:30 +0530

    First Finish

---
+++ myapp/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import Image
+
+class ImageForm(forms.ModelForm):
+    class Meta:
+        model = Image
+        fields = '__all__'
+        labels = {'photo':''}
+
+
+
+
+
